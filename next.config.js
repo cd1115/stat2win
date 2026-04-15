6a112cb6e419c4865f5c5e95f95e0197c373de13
Christian Diaz <christian.059.cd@gmail.com>
2026-04-15 16:20:37 -0400

    Fix: replace next-pwa with @ducanh2912/next-pwa for Next.js 16

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
-const withPWA = require("next-pwa")({
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
   dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
   disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    turbo: {},
-  },
 };
 
-module.exports = withPWA(nextConfig);
+export default withPWA(nextConfig);
